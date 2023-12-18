ecfd88811376f50843314ba28319714cc5b6591b
kinshuk bageja <kinshukbageja@gmail.com>
2023-12-18 15:06:41 +0530

    Image Search engine

---
+++ script.js
@@ -0,0 +1,44 @@
+ const key  = "uV9vpNtYLw_DqRrSsEtv0UKhV77HK-4jpgluyt8BZ7I";
+const  searchbox = document.getElementById("searchbox");
+const  submit2  =  document.getElementById("submit");
+const more2 =  document.getElementById("more2");
+const  showimg = document.getElementById("showimg");
+const  input = document.getElementById("input");
+
+
+
+ let keyword = "";
+ let page = 1;
+
+ async function searchimages(){
+    keyword  = searchbox.value;
+    const url  = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${key}&per_page=12`;
+    const response  = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    const results  = data.results;
+    results.map((result)=>{
+        const image = document.createElement("img");
+        image.src  = result.urls.small;
+        const link = document.createElement("a")
+        link.href =  result.links.html;
+        link.target = "_blank";
+        link.appendChild(image);
+        showimg.appendChild(link);
+    })
+    more2.style.display ="block";
+
+
+
+ }
+ input.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    page = 1;
+    searchimages();
+ });
+ more2.addEventListener("click",()=>{
+    page++;
+    searchimages();
+
+ })
+ 
